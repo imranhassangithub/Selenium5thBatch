ca4f96137d708f16fdf9ac0ec6b6d4b75a4521e9
imranhassangithub <imranhassan.work@gmail.com>
2017-09-12 12:10:06 +0600

    Login with New project structure

--- src/main/java/com/bitm/newtours5th/Utils/UrlTextUtils.java
+++ src/main/java/com/bitm/newtours5th/Utils/UrlTextUtils.java
@@ -3,5 +3,5 @@ package com.bitm.newtours5th.Utils;
 public class UrlTextUtils {
 
 	public static final String BASE_URL = "http://newtours.demoaut.com/";
-	public static final String LOGIN_PAGE_TITLE = "Welcome: Mercury Tours as";
+	public static final String LOGIN_PAGE_TITLE = "Welcome: Mercury Tours";
 }
